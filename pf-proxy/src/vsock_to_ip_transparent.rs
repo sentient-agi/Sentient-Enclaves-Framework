b9a892a0492daa25c93f2cb2e3a7a3d9039bdeec
Andrew Bednoff <andrew@sentient.foundation>
2024-12-19 08:08:58 +0600

    Implementation of VSock to IP transparent proxy/NAT & port forwarder, with retrieving from inbound VSock stream requested resource original destination (`original_dst`) IP, sent through VSock from enclave's transparent proxy.

---
+++ pf-proxy/src/vsock_to_ip_transparent.rs
@@ -0,0 +1,142 @@
+// Based on https://github.com/tokio-rs/tokio/blob/master/examples/proxy.rs
+//
+// Copyright (c) 2022 Tokio Contributors and Marlin Contributors
+//
+// Permission is hereby granted, free of charge, to any
+// person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the
+// Software without restriction, including without
+// limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice
+// shall be included in all copies or substantial portions
+// of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+use anyhow::{anyhow, Context, Result};
+use clap::Parser;
+use futures::FutureExt;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
+use tokio::io;
+use tokio::io::AsyncReadExt;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpStream;
+use tokio_vsock::{VsockAddr, VsockListener, VsockStream};
+
+use pf_proxy::utils;
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    /// vsock address of the listener side, usually open to the other side of the transparent proxy (e.g. 3:1200)
+    #[clap(short, long, value_parser)]
+    vsock_addr: String,
+}
+
+pub async fn proxy(listen_addr: VsockAddr) -> Result<()> {
+    println!("Listening on: {:?}", listen_addr);
+
+    let mut listener = VsockListener::bind(listen_addr)
+        .context("Failed to bind listener to vsock: incorrect CID:port")?;
+
+    while let Ok((inbound, _)) = listener.accept().await {
+        let transfer = transfer(inbound).map(|r| {
+            if let Err(e) = r {
+                println!("Failed to transfer data: error={:?}", e);
+            }
+        });
+
+        tokio::spawn(transfer);
+    }
+
+    Ok(())
+}
+
+async fn transfer(mut inbound: VsockStream) -> Result<()> {
+    let inbound_addr = inbound
+        .peer_addr()
+        .context("could not fetch inbound address from vsock stream")?
+        .to_string();
+
+    let (mut ri, mut wi) = inbound.split();
+
+    // read original destination ip and port through vsock stream sent from enclave's proxy
+    let proxy_addr = match ri.read_u8().await? {
+        4_u8 => Ok(SocketAddr::new(
+            IpAddr::V4(Ipv4Addr::from_bits(ri.read_u32_le().await?)),
+            ri.read_u16_le().await?
+        )),
+        6_u8 => Ok(SocketAddr::new(
+            IpAddr::V6(Ipv6Addr::from_bits(ri.read_u128_le().await?)),
+            ri.read_u16_le().await?
+        )),
+        _ => Err(anyhow!("Can't retrieve original_dst from vsock stream: malformed bytes or its order of original_dst address:port")),
+    }?;
+
+    /*
+    // read original destination ip and port through vsock stream sent from enclave's proxy
+    let proxy_addr = SocketAddr::new(
+        IpAddr::V4(ri.read_u32_le().await?.into()),
+        ri.read_u16_le().await?,
+    );
+    */
+
+    println!("Proxying to: {:?}", proxy_addr);
+
+    let mut outbound = TcpStream::connect(proxy_addr)
+        .await
+        .context("failed to connect to TCP endpoint")?;
+
+    let (mut ro, mut wo) = outbound.split();
+
+    // Send request to upstream resource
+    let client_to_server = async {
+        io::copy(&mut ri, &mut wo)
+            .await
+            .context("error in vsock to ip copy")
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        println!("vsock to ip IO copy done, from {:?} to {:?}", inbound_addr, proxy_addr);
+        wo.shutdown().await
+    };
+
+    // Receive response from upstream resource and write it to inbound connection input stream
+    let server_to_client = async {
+        io::copy(&mut ro, &mut wi)
+            .await
+            .context("error in ip to vsock copy")
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        println!("ip to vsock IO copy done, from {:?} to {:?}", proxy_addr, inbound_addr);
+        wi.shutdown().await
+    };
+
+    tokio::try_join!(client_to_server, server_to_client).with_context(|| {
+        format!(
+            "error in connection between inbound vsock endpoint {:?} and outbound ip address {:?}",
+            inbound_addr, proxy_addr
+        )
+    })?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let vsock_addr = utils::split_vsock(&cli.vsock_addr)?;
+    proxy(vsock_addr).await?;
+
+    Ok(())
+}
