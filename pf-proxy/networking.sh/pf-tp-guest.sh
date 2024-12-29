16fa055c539892fd7661e3e4317bada97c4c31a9
Andrew Bednoff <andrew@sentient.foundation>
2024-12-30 01:39:01 +0600

    Adding scripts for configuring network, make NetFilter rules for ports mapping/forwarding and ports range aggregation for mapping/forwarding them, and running transparent port forwarding proxies, with support of original destination retrieving from TCP stream of request and resend request to retrieved original destination address. Includes `ip-to-vsock-transparent` and `vsock-to-ip-transparent` proxies with NAT. Supports TCP and UDP streams transparent forwarding for various TCP based high-level protocols (including HTTP/HTTPS) and UDP based protocols (including DNS support via `socat` UDP forwarding configuration).

---
+++ pf-proxy/networking.sh/pf-tp-guest.sh
@@ -0,0 +1,66 @@
+##!/bin/bash
+#!/usr/bin/env bash
+
+shopt -s extglob
+shopt -s extquote
+# shopt -s xpg_echo
+
+set -f
+
+mkdir -p /app/pf_build/
+mkdir -p /app/pf_build/.logs/
+cd /app/pf_build/
+
+ip addr add 127.0.0.1/32 dev lo
+ifconfig lo 127.0.0.1
+ip link set dev lo up
+ip route add default dev lo src 127.0.0.1
+echo '127.0.0.1   localhost' | tee /etc/hosts
+# echo '127.0.0.1   wttr.in' | tee -a /etc/hosts
+echo 'nameserver 127.0.0.1' | tee /etc/resolv.conf
+
+nft flush ruleset
+
+# route outgoing packets with a destination other than localhost to a given ip:port
+iptables -t nat -A OUTPUT -p tcp --dport 1:65535 ! -d 127.0.0.1 -j DNAT --to-destination 127.0.0.1:8888
+# iptables -t nat -A OUTPUT -p tcp --dport 443 ! -d 127.0.0.1 -j DNAT --to-destination 127.0.0.1:443
+# iptables -t nat -A OUTPUT -p tcp --dport 80 ! -d 127.0.0.1 -j DNAT --to-destination 127.0.0.1:80
+
+# replace the source address with 127.0.0.1 for outgoing packets with a source of 0.0.0.0
+# ensures returning packets have 127.0.0.1 as the destination and not 0.0.0.0
+iptables -t nat -A POSTROUTING -o lo -s 0.0.0.0 -j SNAT --to-source 127.0.0.1
+
+nft list ruleset
+echo
+iptables-save
+echo
+
+echo -e "ip2vs PIDs:";
+killall -v -9 ip2vs;
+echo -e "ip2vs-tp PIDs:";
+killall -v -9 ip2vs-tp;
+echo -e "p2vs-tp PIDs:";
+killall -v -9 p2vs-tp;
+
+./ip2vs-tp --ip-addr 127.0.0.1:8888 --vsock-addr 3:8888 >> ./.logs/ip2vs-tp.allprotos.output 2>&1 & disown
+## ./ip2vs-tp --ip-addr 127.0.0.1:8888 --vsock-addr 3:8888 2>&1 | tee -a ./.logs/ip2vs-tp.allprotos.output & disown
+
+# ./ip2vs --ip-addr 0.0.0.0:443 --vsock-addr 3:8443 2>&1 | tee -a ./.logs/ip2vs.https.output & disown
+# ./ip2vs-tp --ip-addr 0.0.0.0:443 --vsock-addr 3:8443 2>&1 | tee -a ./.logs/ip2vs-tp.https.output & disown
+## ./ip2vs-tp --ip-addr 127.0.0.1:443 --vsock-addr 3:8443 2>&1 | tee -a ./.logs/ip2vs-tp.https.output & disown
+# ./p2vs-tp --ip-addr 0.0.0.0:443 --vsock 3:8443 2>&1 | tee -a ./.logs/p2vs-tp.https.output & disown
+# ./p2vs-tp --ip-addr 0.0.0.0:443 --vsock 3 2>&1 | tee -a ./.logs/p2vs-tp.https.output & disown
+
+# ./ip2vs --ip-addr 0.0.0.0:80 --vsock-addr 3:8080 2>&1 | tee -a ./.logs/ip2vs.http.output & disown
+# ./ip2vs-tp --ip-addr 0.0.0.0:80 --vsock-addr 3:8080 2>&1 | tee -a ./.logs/ip2vs-tp.http.output & disown
+## ./ip2vs-tp --ip-addr 127.0.0.1:80 --vsock-addr 3:8080 2>&1 | tee -a ./.logs/ip2vs-tp.http.output & disown
+# ./p2vs-tp --ip-addr 0.0.0.0:80 --vsock 3:8080 2>&1 | tee -a ./.logs/p2vs-tp.http.output & disown
+# ./p2vs-tp --ip-addr 0.0.0.0:80 --vsock 3 2>&1 | tee -a ./.logs/p2vs-tp.http.output & disown
+
+echo -e "ip2vs PIDs:";
+pidof ip2vs;
+echo -e "ip2vs-tp PIDs:";
+pidof ip2vs-tp;
+echo -e "p2vs-tp PIDs:";
+pidof p2vs-tp;
+
