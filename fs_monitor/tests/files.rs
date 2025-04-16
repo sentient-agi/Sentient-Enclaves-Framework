f47a009b68c618bccaa9198f3e0c169061c1ed60
Shivraj <shivraj@sentient.foundation>
2025-04-16 13:08:07 +0000

    Adds test for deleting an empty file.

--- fs_monitor/tests/files.rs
+++ fs_monitor/tests/files.rs
@@ -21,9 +21,13 @@ async fn wait_for_file_state(
     while start.elapsed() < timeout {
         // If expected_state is None, we're waiting for the file to be removed
         if expected_state.is_none() {
-            if !file_infos.contains_key(path) {
+            if !file_infos.contains_key(path){
                 eprintln!("File no longer exists in map as expected");
                 return true;
+            }
+            else{
+                let info = file_infos.get(path).unwrap();
+                eprintln!("Found file state: {:?} (expecting None)", info.state);
             }
         } else {
             // Original case - waiting for a specific state
@@ -148,3 +152,37 @@ async fn file_deletion_simple() -> notify::Result<()> {
     Ok(())
 
 }
+
+// Deleting an empty file
+#[tokio::test]
+async fn file_deletion_empty() -> notify::Result<()> {
+    let watch_path = Path::new(".");
+    let mut ignore_list = IgnoreList::new();
+    ignore_list.populate_ignore_list(Path::new("./empty_ignore"));
+
+    let file_infos: Arc<DashMap<String, FileInfo>> = Arc::new(DashMap::new());
+    let hash_infos = Arc::new(HashInfo{
+        ongoing_tasks: Arc::new(tokio::sync::Mutex::new(std::collections::HashMap::new())),
+        hash_results: Arc::new(tokio::sync::Mutex::new(std::collections::HashMap::new())),
+    });
+
+    let _watcher = setup_watcher(
+        watch_path, 
+        Arc::clone(&file_infos), 
+        Arc::clone(&hash_infos),
+        ignore_list
+    ).await?;
+
+    let file_path = format!("test_{}.txt", Uuid::new_v4());
+    let mut file = File::create(file_path.clone())?;
+    let file_path = handle_path(&file_path);
+    eprintln!("Path: {}", file_path);
+    assert!(wait_for_file_state(&file_infos, &file_path, Some(FileState::Created), 1000).await);
+
+    eprintln!("Attempting to delete file: {}", file_path);
+    std::fs::remove_file(file_path.clone()).ok();
+    assert!(wait_for_file_state(&file_infos, &file_path, None, 1000).await);
+    Ok(())
+
+}
+
