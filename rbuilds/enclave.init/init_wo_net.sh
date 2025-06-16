05f831af785ad31d2fb2c8adab9096ae0cb7e925
Andrew Bednoff <andrew@sentient.foundation>
2025-06-16 21:10:12 +0600

    [rbuilds-sys] Change of layout for enclave's init system and init system builds. Introduce `enclave.init` for enclave's init system and init scripts, and `init_apps` for building init system, and enclave's service bus based on NATS, via dedicated Docker container and via Nix build system for init system legacy builds.

---
+++ rbuilds/enclave.init/init_wo_net.sh
@@ -0,0 +1,32 @@
+##!/bin/bash
+#!/usr/bin/env bash
+
+shopt -s extglob
+shopt -s extquote
+# shopt -s xpg_echo
+
+set -f
+
+echo -e "Init.sh started";
+
+# mkdir -vp /apps/.logs/;
+
+echo -e "Executing Pipeline";
+cd /apps/;
+./pipeline listen --port 53000 >> /apps/.logs/pipeline.log 2>&1 & disown;
+
+echo -e "Executing NATS Server for Enclave Bus";
+./nats-server --name "enclave_bus_nats_server"  --addr 127.0.0.1 --port 4222 --http_port 4242 --config ./.config/nats.config --log_size_limit 1073741824 --jetstream >> /apps/.logs/nats-server.log 2>&1 & disown;
+
+echo -e "Executing RA Web Server";
+./ra-web-srv >> /apps/.logs/ra-web-srv.log 2>&1 & disown;
+
+echo -e "Executing FS Monitor";
+./fs-monitor --directory "./" --ignore-file "./.fsignore" --nats-url "nats://127.0.0.1:4222" --kv-bucket-name "fs_hashes" >> /apps/.logs/fs-monitor.log 2>&1 & disown;
+
+# ifconfig -a;
+
+echo -e "Init.sh executed";
+
+tail -f /apps/.logs/pipeline.log
+
